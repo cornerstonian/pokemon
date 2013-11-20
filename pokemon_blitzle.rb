35a3a818c643e2dee0fe68590cbcf95d289538a6
Lavoisier Cornerstone <vois.cornerstone@gmail.com>
2013-11-20 18:54:11 -0500

    initial commit; initialize for 3 Pokemon

---
+++ pokemon_blitzle.rb
@@ -0,0 +1,23 @@
+class Blitzle 
+	attr_accessor :type :abilities :gender_ratio :hatch_time :resistant_to :generation_intro :pokedex_color
+#Blitzle -
+	def initialize
+	@level =
+	@type = "Electric"
+	@abilities = ["Lightingrod", "Motor Drive"]
+	@gender_ratio = "50% male/50% female"
+	@hatch_time = (5355..5609).rand
+	@resistant_to = ["Flying", "Steel", "Electric"]
+	@generation_intro = "V"
+	@pokedex_color = "Black"
+		
+	end
+
+	def method_name
+		
+	end
+
+	def method_name
+		
+	end
+	#Blitzle - http://ht.ly/r1V5M
---
+++ pokemon_dewott.rb
@@ -0,0 +1,23 @@
+
+Class Cofagrigus
+	attr_accessor :type :abilities :gender_ratio :hatch_time :resistant_to :generation_intro :pokedex_color
+
+def initialize
+		@type = "Ghost"
+		@abilities = ["Torrent", "Shell Armor", "Hidden Ability"]
+		@gender_ratio = "87.5% male/12.5% female"
+		@hatch_time = rand(5355..5609)
+		@resistant_to = ["Steel", "Fire", "Water", "Ice"]
+		@generation_intro = "V"
+		@pokedex_color = "Blue"
+		
+	end
+
+	def method_name
+		
+	end
+
+	def method_name
+		
+	end
+#Dewott http://ht.ly/r1PA4
